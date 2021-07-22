eafa758896ce0773c920e2c29c8e5366bc57d783
saurabh12571257 <87791567+saurabh12571257@users.noreply.github.com>
2021-07-22 08:24:02 +0530

    Add files via upload

---
+++ 01.saurabh.py
@@ -0,0 +1,40 @@
+import random 
+import os
+os.uname()
+
+def generate():
+    number = random.randint(1,6)
+    print("Dice Rolling Simulator")
+    print("Made by saurabh dave")
+    if number == 1:
+        print("1\\n")
+    elif number == 2:
+        print("2\\n")
+    elif number == 3:
+        print("3")
+    elif number == 4:
+        print("4\\n")
+    elif number == 5:
+        print("5\\n")
+    elif number == 6:
+        print("6\\n")
+    return
+
+print('To roll again enter "y" \nTo exit enter "n"')
+
+while True:
+    choice = input()
+    if choice == "y" or choice == "Y":
+        os.uname()
+        generate()
+        print('To roll again enter "y"\nTo exit app enter "n"')
+    elif choice == "n" or choice == "N":
+        os.uname()
+        exit()
+    else:
+        print("Wrong input, try again")
+
+
+
+
+
